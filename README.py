d6d65ffb1995a11238f05b6c26ff89e24d4f6833
chittha b s <69895777+chitthabs@users.noreply.github.com>
2020-08-19 14:22:03 +0530

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,22 @@
+# 10-days-of-code
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+    shifted = a[d%n:] + a[:d%n]
+    for num in shifted:
+        print(num, end=' ')
